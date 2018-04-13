9e81c1ccc74b750aa806686bcd2cc06db84cd8e9
Henry Richardson <hrichardson83@gmail.com>
2018-04-12 22:56:15 -0400

    added code for firebase.

--- please-introduce-yourself/src/App.vue
+++ please-introduce-yourself/src/App.vue
@@ -1,14 +1,40 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div v-for="message in messages">
+      <h4>{{ message.title }}</h4>
+      <p>{{ message.text }}</p>
+      <p>{{ message.timestamp }}</p>
+    </div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase.js"></script>
 <script>
+  // Initialize Firebase
+
+</script>
+<script>
+import Firebase from 'firebase'
+
+let config = {
+  apiKey: "AIzaSyCvg-CVhjGw0nwkcO1BO7a0VhZkOIILRbw",
+  authDomain: "pleaseintroduceyourself-70e90.firebaseapp.com",
+  databaseURL: "https://pleaseintroduceyourself-70e90.firebaseio.com",
+  projectId: "pleaseintroduceyourself-70e90",
+  storageBucket: "pleaseintroduceyourself-70e90.appspot.com",
+  messagingSenderId: "736569888183"
+};
+let app = Firebase.initializeApp(config)
+let db = app.database()
+let messagesRef = db.ref('messages')
 export default {
-  name: 'App'
+  name: 'app',
+  firebase: {
+    messages: messagesRef
+  }
 }
+
 </script>
 
 <style>
